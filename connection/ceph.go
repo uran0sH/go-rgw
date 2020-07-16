099969cfa504ee67a54892f11a55e20a01b4eff0
RobotHuang <1183598761@qq.com>
2020-07-16 21:47:38 +0800

    feat: init pools

--- connection/ceph.go
+++ connection/ceph.go
@@ -7,8 +7,8 @@ import (
 
 type Ceph struct {
 	Connection *rados.Conn
+	Pools      map[string]bool
 }
-
 
 func NewCeph() (*Ceph, error) {
 	conn, err := rados.NewConn()
@@ -17,6 +17,7 @@ func NewCeph() (*Ceph, error) {
 	}
 	ceph := &Ceph{
 		Connection: conn,
+		Pools:      make(map[string]bool),
 	}
 	return ceph, nil
 }
@@ -31,9 +32,74 @@ func (c *Ceph) InitDefault() error {
 		return err
 	}
 	fmt.Println("connect ceph cluster successfully")
+	err = c.InitPools()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
+const (
+	// rgw.bucket.data stores the object
+	BucketData = "rgw.bucket.data"
+
+	// rgw.user.uid stores the uid of user and id of user's bucket
+	UserUid = "rgw.user.uid"
+
+	// rgw.bucket.index stores the index of bucket
+	BucketIndex = "rgw.bucket.index"
+)
+
+// InitPools creates the pools the go-rgw needs
+func (c *Ceph) InitPools() error {
+	existedPools, err := c.Connection.ListPools()
+	if err != nil {
+		return err
+	}
+	// determine whether pools already have existed
+	for _, value := range existedPools {
+		if value == BucketData {
+			c.Pools[BucketData] = true
+		}
+		if value == UserUid {
+			c.Pools[UserUid] = true
+		}
+		if value == BucketIndex {
+			c.Pools[BucketIndex] = true
+		}
+	}
+	// if pool doesn't exist, create the pool.
+	if _, ok := c.Pools[BucketData]; !ok || !c.Pools[BucketData] {
+		err := c.createPool(BucketData)
+		if err != nil {
+			return err
+		}
+		c.Pools[BucketData] = true
+	}
+	if _, ok := c.Pools[UserUid]; !ok || !c.Pools[UserUid] {
+		err := c.createPool(UserUid)
+		if err != nil {
+			return err
+		}
+		c.Pools[UserUid] = true
+	}
+	if _, ok := c.Pools[BucketIndex]; !ok || !c.Pools[BucketIndex] {
+		err := c.createPool(BucketIndex)
+		if err != nil {
+			return err
+		}
+		c.Pools[BucketIndex] = true
+	}
+	return nil
+}
+
+// create pool
+func (c *Ceph) createPool(name string) error {
+	err := c.Connection.MakePool(name)
+	return err
+}
+
+// close the connection to the Ceph cluster
 func (c *Ceph) Shutdown() {
 	if c.Connection != nil {
 		c.Connection.Shutdown()
@@ -52,7 +118,6 @@ func (c *Ceph) WriteObject(pool string, oid string, data []byte, offset uint64) 
 	}
 	return nil
 }
-
 
 func (c *Ceph) ReadObject(pool string, oid string, data []byte, offset uint64) (int, error) {
 	ioctx, err := c.Connection.OpenIOContext(pool)
