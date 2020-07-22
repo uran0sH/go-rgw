a5b2a4354b78376cb26e3b54a494fe1292f0ac7d
RobotHuang <1183598761@qq.com>
2020-07-22 21:37:41 +0800

    feat: allocate objectID

--- allocator/id.go
+++ allocator/id.go
@@ -1,13 +1,33 @@
 package allocator
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"go-rgw/connection"
+	"strings"
+)
 
-func AllocateID() string {
-	oid := uuid.New()
-	return oid.String()
+// AllocateObjectID
+func AllocateObjectID(bucketID, clusterID string) string {
+	objectUUID := uuid.New()
+	objectID := strings.Join([]string{clusterID, bucketID, objectUUID.String()}, ".")
+	return objectID
 }
 
-func AllocateMetadataID() string {
-	oid := uuid.New()
-	return "metadata_" + oid.String()
+// AllocateBucketID
+func AllocateBucketID() string {
+	bucketID := uuid.New()
+	return bucketID.String()
+}
+
+func AllocateMetadataID(objectID string) string {
+	return objectID + "-metadata"
+}
+
+func AllocateAclID(objectID string) string {
+	return objectID + "-acl"
+}
+
+// GetClusterID
+func GetClusterID() (string, error) {
+	return connection.CephMgr.Ceph.Connection.GetFSID()
 }
