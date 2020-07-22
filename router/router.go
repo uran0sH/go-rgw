50b46806b062e9b3f5312763e99faf2f67bd1e61
RobotHuang <1183598761@qq.com>
2020-07-22 21:41:22 +0800

    refactor: remove jwt

--- router/router.go
+++ router/router.go
@@ -6,13 +6,15 @@ import (
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	r.POST("/register", register)
-	r.POST("/login", login)
-	authorized := r.Group("/")
-	authorized.Use(JWTAuthMiddleware())
-	{
-		authorized.POST("/upload", putObject)
-		authorized.GET("/download", getObject)
-	}
+	//r.POST("/register", register)
+	//r.POST("/login", login)
+	//authorized := r.Group("/")
+	//authorized.Use(JWTAuthMiddleware())
+	//{
+	//	authorized.POST("/upload", putObject)
+	//	authorized.GET("/download", getObject)
+	//}
+	r.POST("/upload", putObject)
+	r.POST("/download", getObject)
 	return r
 }
