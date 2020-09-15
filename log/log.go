76304347b0b17b501b286321f3c2c47a2617169c
RobotHuang <1183598761@qq.com>
2020-09-15 20:07:36 +0800

    feat:log

---
+++ log/log.go
@@ -0,0 +1,22 @@
+package log
+
+import (
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+)
+
+var Log = logrus.New()
+
+func Init(filename string) error {
+	logFile, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0777)
+	if err != nil {
+		return err
+	}
+	Log.SetOutput(io.MultiWriter(os.Stdout, logFile))
+	Log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	Log.SetReportCaller(true)
+	return nil
+}
